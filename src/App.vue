2e9f54b622ddfa88ae35dbdcfde0d175f0260275
Washington Daniel <washingtondaniel320@gmail.com>
2023-11-03 01:12:44 -0300

    Creating button component

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,14 @@
 <template>
   <main>
-    Home
+    <IButton title="23232323"/>
     <div>teste</div>
   </main>
 </template>
+<script lang="ts">
+import { defineComponent } from 'vue';
+import IButton from './components/button/IButton.vue';
+
+export default defineComponent({
+  components: { IButton }
+})
+</script>
---
+++ src/components/button/IButton.vue
@@ -0,0 +1,17 @@
+<template>
+    <button>{{title}}</button>
+</template>
+<script lang="ts">
+import { defineComponent } from 'vue';
+export default defineComponent({
+  props: {
+
+    title: {
+        type: String,
+
+     
+    }
+    
+  }
+})
+</script>
